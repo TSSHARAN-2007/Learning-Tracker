d12077fc6c989ea41f8075783aa8673e4f3ab844
T S SHARAN <tssharanofficial@gmail.com>
2026-08-14 23:33:17 +0530

    Update service worker to cache additional files
    Added manifest.json and icon.png to cache during installation.

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,11 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("tracker-cache").then(cache => {
-      return cache.addAll(["index.html"]);
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon.png"
+      ]);
     })
   );
 });
